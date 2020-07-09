9aa125f5b1b1919c9f1f3a0399fbd9e84dbe5c5c
Caroline Lester <carolineclester@gmail.com>
2020-07-08 23:37:37 -0400

    initial code for friend finder

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Claire",
+		photo: "https://www.linkedin.com/in/rickardclaire/detail/photo/",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Monica",
+		photo: "https://www.linkedin.com/in/monicasain/detail/photo/",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Kasey",
+		photo: "https://www.linkedin.com/in/kasey-hart-611705a4/detail/photo/",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Mary",
+		photo: "https://www.linkedin.com/in/mary-cummings000/detail/photo/",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Jasmyn",
+		photo: "https://www.linkedin.com/in/jasmyn-woods-91806713b/detail/photo/",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
