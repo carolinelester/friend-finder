fa615b5a7cf908cb588cdd6645dddde11a10a357
Caroline Lester <carolineclester@gmail.com>
2020-07-09 23:35:40 -0400

    update to gitignore

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,11 @@
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
+  app.get('/api/friends', function(req, res) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post('/api/friends', function(req, res) {
     console.log(req.body.scores);
 
     var user = req.body;
--- server.js
+++ server.js
@@ -8,14 +8,12 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "../app/css"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // API and HTML routes
-require("../app/routing/apiRoutes.js")(app);
-require("../app/routing/htmlRoutes.js")(app);
+require("app/routing/apiRoutes.js")(app);
+require("app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
